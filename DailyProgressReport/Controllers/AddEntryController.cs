6ade6bd045878f9e1d897de3273d6d1665295513
Rockpock <rockpocktechnologies.com>
2024-02-15 18:15:19 +0530

    15-02-24

--- DailyProgressReport/Controllers/AddEntryController.cs
+++ DailyProgressReport/Controllers/AddEntryController.cs
@@ -5,6 +5,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data.SqlClient;
 using System.Data;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace DailyProgressReport.Controllers
 {
@@ -24,8 +29,11 @@ namespace DailyProgressReport.Controllers
             {
                 AddEntryModel model = new AddEntryModel();
                 model.Projects = GetProjects();
+                //model.entries = GetAddEntriesFromDatabase();
                 //return View( model);
                 List<AddEntryModel> projects = GetProjects();
+                List<AddEntryModel> entries = GetAddEntriesFromDatabase();
+
                 return View(model);
             }
             catch (Exception ex)
@@ -37,6 +45,68 @@ namespace DailyProgressReport.Controllers
                 errorLogger.LogError(ex, exceptionMessage);
                 return Json(new { error = ex.Message });
             }
+
+        }
+        public IActionResult EntryList()
+        {
+            List<AddEntryModel> entries = GetAddEntriesFromDatabase();
+            ViewBag.Entries = entries;
+            return View();
+        }
+        private List<AddEntryModel> GetentryListFromDatabase()
+        {
+            List<AddEntryModel> entries = new List<AddEntryModel>();
+
+            try
+            {
+                string loggedInUserId = HttpContext.Session.GetString("SLoggedInUserID");
+
+                if (loggedInUserId != null)
+                {
+                    CommonFunction cmn = new CommonFunction();
+                    using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ConnectionString")))
+                    {
+                        connection.Open();
+
+                        using (SqlCommand command = new SqlCommand("dpr_sp_GetBlocksByUser", connection))
+                        {
+                            command.CommandType = CommandType.StoredProcedure;
+                            command.Parameters.AddWithValue("@Username", loggedInUserId);
+
+                            using (SqlDataReader reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    AddEntryModel entry = new AddEntryModel
+                                    {
+                                        Id = (int)reader["Id"],
+                                        //ProjectName = reader["ProjectName"].ToString(),
+                                        BlockQuantity = reader["BlockQuantity"] != DBNull.Value ? (int)reader["BlockQuantity"] : 0,
+                                        JTDQuantity = reader["JTDQuantity"] != DBNull.Value ? (int)reader["JTDQuantity"] : 0,
+                                        Date = cmn.ConvertDateFormatmmddyytoddmmyyDuringDisplay(reader["Date"].ToString()),
+                                        
+                                    };
+
+                                    entries.Add(entry);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                string exceptionMessage = $"Exception in method '{methodName}'";
+
+                // Log or rethrow the exception with the updated message
+                var errorLogger = new CustomErrorLog(_configuration);
+                errorLogger.LogError(ex, exceptionMessage);
+                //return Json(new { error = ex.Message });
+            }
+            return entries;
 
         }
         private List<AddEntryModel> GetProjects()
@@ -72,5 +142,161 @@ namespace DailyProgressReport.Controllers
 
             return projects;
         }
+
+        [HttpPost]
+        public ActionResult AddEntryToDatabase( int BlockQuantity, int BOQReferenceID, int jtdQuantity, DateTime Date)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ConnectionString")))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("INSERT INTO dpr_MaterialTransactions ( BlockQuantity, BOQReferenceID, JTDQuantity,Date) VALUES ( @BlockQuantity, @BOQReferenceID, @JTDQuantity,@Date)", connection))
+                    {
+                        // Add parameters to the SQL command
+                        command.Parameters.AddWithValue("@BlockQuantity", Convert.ToInt32 (BlockQuantity));
+                        command.Parameters.AddWithValue("@BOQReferenceID", BOQReferenceID);
+                        command.Parameters.AddWithValue("@JTDQuantity", jtdQuantity);
+                        command.Parameters.AddWithValue("@Date", Date);
+
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return Json(new { success = true, message = "Entry added successfully" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Error adding Entry", error = ex.Message });
+            }
+        }
+            [HttpPost]
+            public IActionResult AddEntry( int BlockQuantity,int BOQReferenceID, int jtdQuantity)
+            {
+                try
+                {
+                    string loggedInUserId = HttpContext.Session.GetString("SLoggedInUserID");
+                    if (loggedInUserId != null && loggedInUserId != "")
+                    {
+                    DateTime Date = DateTime.Now;
+
+                    AddEntryToDatabase( BlockQuantity, BOQReferenceID, jtdQuantity, Date); // Replace "User123" with the actual user or logged-in user
+                        return Json(new { success = true, message = "Entry added successfully!" });
+                    }
+                    else
+                    {
+                        return Json(new { success = false, message = $"Error adding Entry, please login and try again." });
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                    string exceptionMessage = $"Exception in method '{methodName}'";
+
+                    // Log or rethrow the exception with the updated message
+                    var errorLogger = new CustomErrorLog(_configuration);
+                    errorLogger.LogError(ex, exceptionMessage);
+                    return Json(new { error = ex.Message });
+                }
+            }
+        private List<AddEntryModel> GetProjectsFromDatabase()
+        {
+            List<AddEntryModel> projects = new List<AddEntryModel>();
+
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM dpr_MaterialTransactions ORDER BY Id DESC", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            AddEntryModel project = new AddEntryModel
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                ProjectName = reader["ProjectName"].ToString(),
+                            };
+
+                            projects.Add(project);
+                        }
+                    }
+                }
+            }
+
+            return projects;
+        }
+        [HttpGet]
+        public IActionResult GetAddEntries()
+        {
+            try
+            {
+                List<AddEntryModel> entries = GetAddEntriesFromDatabase();
+                return Json(new { success = true, entries });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, error = ex.Message });
+            }
+        }
+
+        private List<AddEntryModel> GetAddEntriesFromDatabase()
+        {
+            List<AddEntryModel> entries = new List<AddEntryModel>();
+            CommonFunction cmn = new CommonFunction();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ConnectionString")))
+                {
+
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SELECT Id,BlockQuantity,BOQReferenceID,JTDQuantity,Date FROM dpr_MaterialTransactions", connection))
+                    {
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+
+                            while (reader.Read())
+                            {
+
+                                AddEntryModel entry = new AddEntryModel
+                                {
+
+                                    Id = (int)reader["Id"],
+                                    BlockQuantity = reader["BlockQuantity"] != DBNull.Value ? (int)reader["BlockQuantity"] : 0,
+                                    BOQReferenceID = reader["BOQReferenceID"] != DBNull.Value ? (int)reader["BOQReferenceID"] : 0,
+                                    JTDQuantity = reader["JTDQuantity"] != DBNull.Value ? (int)reader["JTDQuantity"] : 0,
+                                    Date = reader["Date"] != DBNull.Value ? cmn.ConvertDateFormatmmddyytoddmmyyDuringDisplay(reader["Date"].ToString()) : "",
+
+                                };
+
+                                entries.Add(entry);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                string exceptionMessage = $"Exception in method '{methodName}'";
+
+                // Log or rethrow the exception with the updated message
+                var errorLogger = new CustomErrorLog(_configuration);
+                errorLogger.LogError(ex, exceptionMessage);
+                //return Json(new { error = ex.Message });
+            }
+
+            return entries;
+        }
+
+
     }
 }
+
--- DailyProgressReport/Controllers/Models/AddEntryModel.cs
+++ DailyProgressReport/Controllers/Models/AddEntryModel.cs
@@ -2,13 +2,17 @@
 {
     public class AddEntryModel
     {
-        public List<ProjectViewModel> projects { get; set; }
 
         public int Id { get; set; }
         public int ProjectID { get; set; }
         public string ProjectName { get; set; }
         public List<AddEntryModel> Projects { get; set; }
+        public int BlockQuantity { get; set; }
+        public int JTDQuantity { get; set; }
+        public string Date { get; set; }
+        public int BOQReferenceID { get; set; }
 
+        
 
     }
 }
